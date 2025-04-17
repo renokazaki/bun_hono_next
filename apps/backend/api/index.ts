52b0f67d60f95343c92180762a98b5fbccd94a54
ren <ren84690@gmail.com>
2025-04-17 14:10:05 +0900

    バックエンドの開発用のnode-serverのコメントアウト

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { handle } from "hono/vercel";
 //開発用
-import { serve } from "@hono/node-server";
+// import { serve } from "@hono/node-server";
 
 export const config = {
   runtime: "edge",
@@ -17,14 +17,14 @@ app.use(
   })
 );
 
-//開発用
-const port = 8085;
-console.log(`Server is running on http://localhost:${port}`);
+// //開発用
+// const port = 8085;
+// console.log(`Server is running on http://localhost:${port}`);
 
-serve({
-  fetch: app.fetch,
-  port,
-});
+// serve({
+//   fetch: app.fetch,
+//   port,
+// });
 
 const route = app.get("/hello", (c) => {
   return c.json({ message: "Hello Hono!" });
