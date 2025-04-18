8a8db7a1cdc6d8d536bb455f3da37b4e7bbd002e
ren <ren84690@gmail.com>
2025-04-18 10:11:17 +0900

    hello prismaも追加

---
+++ apps/backend/prisma/migrations/20250417232250_init/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Todo` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Todo";
+
+-- CreateTable
+CREATE TABLE "todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "todo_pkey" PRIMARY KEY ("id")
+);
