f8a2b88781692db9c10bc8848f57209f5a5e1d79
ren <ren84690@gmail.com>
2025-04-18 14:44:28 +0900

    prisma動作確認

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -4,14 +4,19 @@ import { handle } from "hono/vercel";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-// //開発用
-// import { serve } from "@hono/node-server";
-// データベースURLを直接指定
-const DATABASE_URL = process.env.DATABASE_URL;
+//👷開発用
+//import { serve } from "@hono/node-server";
 
 export const config = {
   runtime: "edge",
 };
+
+//👷開発用データベースURLを直接指定
+//const DATABASE_URL =
+//"prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZjAzMjcyYzctYzNjYS00NWQzLTg4ZDktNjIwZmU2NTkzNTdjIiwidGVuYW50X2lkIjoiZjA4Y2Y2ZWRkYjY3OGQ5ZTgzYThiZGY5MmMyNzdjNzdmM2FkZTBjOGIwOTI0MGJiYTVmOGQ4YWY3ZjYwNmExZSIsImludGVybmFsX3NlY3JldCI6ImNiYzg1NzNmLTNjNWQtNDc1Zi05YzY0LWIzMjVkZGYwNjgwNSJ9.tOT0f2SLpM9edC4e3_mAUEE9JbK62_j8wS96aRotwp8";
+
+//🔥本番用
+const DATABASE_URL = process.env.DATABASE_URL;
 
 // Create the main Hono app
 const app = new Hono().basePath("/api");
@@ -26,49 +31,47 @@ app.use(
   })
 );
 
-// 開発用
-// const port = 8085;
-// console.log(`Server is running on http://localhost:${port}`);
+// 👷開発用
+//const port = 8085;
+//console.log(`Server is running on http://localhost:${port}`);
 
-// serve({
-//   fetch: app.fetch,
-//   port,
-// });
+//serve({
+//  fetch: app.fetch,
+//  port,
+//});
 
-const route = app.get("/hello", (c) => {
+const hello = app.get("/hello", (c) => {
   return c.json({ message: "Hello Hono!" });
 });
 
-app.get("/test", (c) => {
-  return c.json({ message: "test Hono!" });
-});
-
-app.get("/todos", async (c) => {
+const getTodo = app.get("/todos", async (c) => {
   const gettodos = await prisma.todo.findMany();
   return c.json(gettodos);
 });
 
-// // Todoエンドポイント
-// app.get("/todos", async (c) => {
+// const postTodo = app.post("/todos", async (c) => {
 //   try {
-//     // Prisma Clientを初期化
-//     const prisma = new PrismaClient({
-//       datasourceUrl: DATABASE_URL,
-//     }).$extends(withAccelerate());
+//     const body = await c.req.json();
 
-//     const todos = await prisma.todo.findMany();
-//     return c.json(todos);
+//     const newTodo = await prisma.todo.create({
+//       data: {
+//         title: body.title,
+//         completed: body.completed,
+//       },
+//     });
+
+//     return c.json(newTodo, 201);
 //   } catch (error) {
-//     console.error("Error fetching todos:", error);
+//     console.error("Error creating todo:", error);
 //     return c.json(
 //       {
-//         error: "Failed to fetch todos",
+//         error: "Failed to create todo",
 //       },
 //       500
 //     );
 //   }
 // });
 
-export type AppType = typeof route;
+export type AppType = typeof hello & typeof getTodo;
 
 export default handle(app);
