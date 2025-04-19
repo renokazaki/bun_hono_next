0f8c254174eb6ad1a071cce512da36f0563ae463
ren <ren84690@gmail.com>
2025-04-19 17:28:00 +0900

    appをbooksに変えてみた

--- apps/backend/api/books.ts
+++ apps/backend/api/books.ts
@@ -1,12 +1,10 @@
 // books.ts
 import { Hono } from "hono";
-export const config = {
-  runtime: "edge",
-};
-const app = new Hono()
+
+const books = new Hono()
 
   .get("/", (c) => c.json("list books"))
   .post("/", (c) => c.json("create a book", 201))
   .get("/:id", (c) => c.json(`get ${c.req.param("id")}`));
 
-export default app;
+export default books;
