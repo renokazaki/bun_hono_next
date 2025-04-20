e02acad7fb8da71c0e6ec5396ca820834706532c
ren <ren84690@gmail.com>
2025-04-20 13:16:43 +0900

    gettodoの修正

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -26,14 +26,19 @@ const app = new Hono()
   .get("/hello", (c) => {
     return c.json({ message: "Hello Hono!" });
   })
-  .get("/todos", async (c) => {
-    const gettodos = await prisma.todo.findMany();
-    return c.json(gettodos);
-  })
+  // .get("/todos", async (c) => {
+  //   const gettodos = await prisma.todo.findMany();
+  //   return c.json(gettodos);
+  // })
 
   // .route("/todos", todos) // Handle /user
   .route("/user", user) // Handle /user
   .route("/book", book); // Handle /book
+
+const getTodo = app.get("/todos", async (c) => {
+  const gettodos = await prisma.todo.findMany();
+  return c.json(gettodos);
+});
 
 // 👷開発用
 //const port = 8085;
@@ -44,7 +49,7 @@ const app = new Hono()
 //  port,
 //});
 
-export type AppType = typeof app;
+export type AppType = typeof app & typeof getTodo;
 export type TodosType = typeof todos;
 
 export default handle(app);
