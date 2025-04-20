8bc851a612d96400f68ce9c659beda5efd70aed4
ren <ren84690@gmail.com>
2025-04-20 13:34:49 +0900

    edgeを追加

--- apps/backend/api/testaa.ts
+++ apps/backend/api/testaa.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono";
 import { prisma } from "../prisma/prisma";
 
+export const config = {
+  runtime: "edge",
+};
 const testaa = new Hono();
 
 testaa.get("/todos", async (c) => {
