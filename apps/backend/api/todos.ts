3008de30b52c47c9ec0697bd50ed1573dcb6ecb7
ren <ren84690@gmail.com>
2025-04-20 13:18:20 +0900

    todo.tsのコメントアウト

--- apps/backend/api/todos.ts
+++ apps/backend/api/todos.ts
@@ -1,46 +1,46 @@
-import { Hono } from "hono";
-import { cors } from "hono/cors";
+// import { Hono } from "hono";
+// import { cors } from "hono/cors";
 
-import { prisma } from "../prisma/prisma";
+// import { prisma } from "../prisma/prisma";
 
-// export const config = {
-//   runtime: "edge",
-// };
+// // export const config = {
+// //   runtime: "edge",
+// // };
 
-const todos = new Hono()
-  .basePath("/test")
-  //   .use(
-  //     "*",
-  //     cors({
-  //       origin: "*",
-  //     })
-  //   )
-  .get("/todos", async (c) => {
-    const gettodos = await prisma.todo.findMany();
-    return c.json(gettodos);
-  })
+// const todos = new Hono()
+//   .basePath("/test")
+//   //   .use(
+//   //     "*",
+//   //     cors({
+//   //       origin: "*",
+//   //     })
+//   //   )
+//   .get("/todos", async (c) => {
+//     const gettodos = await prisma.todo.findMany();
+//     return c.json(gettodos);
+//   })
 
-  .post("/todos", async (c) => {
-    try {
-      const body = await c.req.json();
+//   .post("/todos", async (c) => {
+//     try {
+//       const body = await c.req.json();
 
-      const newTodo = await prisma.todo.create({
-        data: {
-          title: body.title,
-          completed: body.completed,
-        },
-      });
+//       const newTodo = await prisma.todo.create({
+//         data: {
+//           title: body.title,
+//           completed: body.completed,
+//         },
+//       });
 
-      return c.json(newTodo, 201);
-    } catch (error) {
-      console.error("Error creating todo:", error);
-      return c.json(
-        {
-          error: "Failed to create todo",
-        },
-        500
-      );
-    }
-  });
+//       return c.json(newTodo, 201);
+//     } catch (error) {
+//       console.error("Error creating todo:", error);
+//       return c.json(
+//         {
+//           error: "Failed to create todo",
+//         },
+//         500
+//       );
+//     }
+//   });
 
-export default todos;
+// export default todos;
