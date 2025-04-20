eb0f7b2faa0e3d99acc2db92f238d4ee9879374d
ren <ren84690@gmail.com>
2025-04-20 13:37:05 +0900

    rootが間違ってただけ。個別のedgemも消してみた

--- apps/backend/api/testaa.ts
+++ apps/backend/api/testaa.ts
@@ -1,12 +1,9 @@
 import { Hono } from "hono";
 import { prisma } from "../prisma/prisma";
 
-export const config = {
-  runtime: "edge",
-};
 const testaa = new Hono();
 
-testaa.get("/todos", async (c) => {
+testaa.get("/", async (c) => {
   const gettodos = await prisma.todo.findMany();
   return c.json(gettodos);
 });
