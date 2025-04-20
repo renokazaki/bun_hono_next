4372c60752c720e04529af7ec8bbb005e7b7bb03
ren <ren84690@gmail.com>
2025-04-20 13:46:54 +0900

    個別のedge消してみた

--- apps/backend/api/testaa.ts
+++ apps/backend/api/testaa.ts
@@ -1,9 +1,6 @@
 import { Hono } from "hono";
 import { prisma } from "../prisma/prisma";
 
-export const config = {
-  runtime: "edge",
-};
 const testaa = new Hono();
 
 testaa.get("/todos", async (c) => {
